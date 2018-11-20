6aa9b20b792d548456e4b2bae64eeb71cbc5d6af
Dave <davewang@users.noreply.github.com>
2018-11-20 21:28:05 +0800

    Projections and Coordinate Systems

--- GLFWOpenGLFirst/main.cpp
+++ GLFWOpenGLFirst/main.cpp
@@ -62,51 +62,122 @@ int main()
 
 	glViewport(0, 0, screenWidth, screenHeight);
 	 
+	glEnable(GL_DEPTH_TEST);
+
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
 
 	Shader ourShader("core.vs", "core.frag");
 	 
-	GLfloat vertex[] = 
-	{ 
-		// Positions          // Colors           // Texture Coords
-		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
-		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
-		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
-		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left
+	// Set up vertex data (and buffer(s)) and attribute pointers
+	// use with Orthographic Projection
+	/*GLfloat vertices[] = {
+	-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 0.0f,
+	0.5f * 500, -0.5f * 500, -0.5f * 500,  1.0f, 0.0f,
+	0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
+	0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
+	-0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
+	-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 0.0f,
+
+	-0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
+	0.5f * 500, -0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 1.0f,
+	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 1.0f,
+	-0.5f * 500,  0.5f * 500,  0.5f * 500,  0.0f, 1.0f,
+	-0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
+
+	-0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+	-0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
+	-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
+	-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
+	-0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
+	-0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+
+	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+	0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
+	0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
+	0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
+	0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
+	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+
+	-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
+	0.5f * 500, -0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
+	0.5f * 500, -0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+	0.5f * 500, -0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+	-0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
+	-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
+
+	-0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
+	0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
+	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+	-0.5f * 500,  0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
+	-0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 1.0f
+	};  */
+	// use with Perspective Projection
+		GLfloat vertices[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
-	GLuint indices[] =
-	{
-		0, 1, 3, // First Triangle
-		1, 2, 3  // Second Triangle
-	};
 
-	GLuint VBO, VAO, EBO;
+	GLuint VBO, VAO;
 	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	glGenBuffers(1, &VBO); 
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex), vertex, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	// 位置属性
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-	// 颜色属性
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat) ));
-	glEnableVertexAttribArray(1);
+ 
 	// 纹理坐标属性
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(2);
 
 	// 释放
-	//glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
 	GLuint texture;
@@ -121,13 +192,15 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	unsigned char *image = SOIL_load_image("res/images/image2.png", &width, &height, 0, SOIL_LOAD_RGBA);
+	unsigned char *image = SOIL_load_image("res/images/image1.jpg", &width, &height, 0, SOIL_LOAD_RGBA);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	SOIL_free_image_data(image);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
-
+	glm::mat4 projection;
+	projection = glm::perspective(45.0f,(GLfloat)screenWidth/ (GLfloat)screenHeight,0.1f,1000.0f);
+	//projection = glm::ortho(0.0f, (GLfloat)screenWidth, 0.0f, (GLfloat)screenHeight, 0.1f, 1000.0f);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -137,26 +210,32 @@ int main()
 	    // Render
 		// Clear the colorbuffer
 		glClearColor(0.2f,0.3f,0.3f,1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		
-		// Draw the triangle
-		ourShader.Use();
-
-		// Create transformations
-		glm::mat4 transform;
-		transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
-		transform = glm::rotate(transform, (GLfloat)glfwGetTime() * -5.0f, glm::vec3(0.0f, 0.0f, 1.0f));
-
-		// Get matrix's uniform location and set matrix
-		GLint transformLocation = glGetUniformLocation(ourShader.Program, "transform");
-		glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(transform));
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture);
 		glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture"), 0);
 
+		ourShader.Use();
+ 
+		glm::mat4 model;
+		glm::mat4 view;
+		model = glm::rotate(model, (GLfloat)glfwGetTime()*1.0f, glm::vec3(0.5f, 1.0f, 0.0f));
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		//model = glm::rotate(model, (GLfloat)glfwGetTime()*0.5f, glm::vec3(1.0f, 0.5f, 0.0f));
+		//view = glm::translate(view, glm::vec3(screenWidth / 2, screenHeight / 2, -700.0f));
+
+
+		GLint modelLoc = glGetUniformLocation(ourShader.Program,"model");
+		GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
+		GLint projectionLoc = glGetUniformLocation(ourShader.Program, "projection");
+
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 		glBindVertexArray(0);
 
 		/* Swap front and back buffers */
@@ -165,8 +244,7 @@ int main()
 		
 	}
 	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
+	glDeleteBuffers(1, &VBO); 
 	glfwTerminate();
 	return EXIT_SUCCESS;
 }
