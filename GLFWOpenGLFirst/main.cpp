aa7ad2bfa0ad99da5374af655be99870f951e31b
Dave <davewang@users.noreply.github.com>
2018-11-20 16:14:16 +0800

    Load Textures

--- GLFWOpenGLFirst/main.cpp
+++ GLFWOpenGLFirst/main.cpp
@@ -12,6 +12,7 @@
 #include <GLFW/glfw3.h>
 
 #include "Shader.h"
+#include "SOIL2\SOIL2.h"
 
 const GLint SCREENT_WIDTH = 800, SCREENT_HEIGHT = 600;
 
@@ -57,35 +58,71 @@ int main()
 
 	glViewport(0, 0, screenWidth, screenHeight);
 	 
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
 
 	Shader ourShader("core.vs", "core.frag");
 	 
 	GLfloat vertex[] = 
 	{ 
-		//postion               //color
-		-0.5f, -0.5f, 0.0f,	1.0f,0.0f,0.0f,	 // Left
-		0.5f, -0.5f, 0.0f,		0.0f,1.0f,0.0f,// Right
-		0.0f,  0.5f, 0.0f,		0.0f,0.0f,1.0f// Top
+		// Postion              // Color			// Texture Coordinates
+		0.5f, 0.5f, 0.0f,		1.0f,0.0f,0.0f,		1.0f,1.0f,	// Top
+		0.5f, -0.5f, 0.0f,		1.0f,1.0f,1.0f,		1.0f,0.0f,  // Right
+		-0.5f,-0.5f, 0.0f,		1.0f,0.0f,0.0f,		0.0f,0.0f,  // Left
+		-0.5f, 0.5f, 0.0f,		1.0f,0.0f,1.0f,		0.0f,1.0f	//
 	};
 
-	GLuint VBO, VAO;
+	GLuint indices[] =
+	{
+		0, 1, 3, // First Triangle
+		1, 2, 3  // Second Triangle
+	};
+
+	GLuint VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex), vertex, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// 位置属性
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat) ));
+	// 颜色属性
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat) ));
 	glEnableVertexAttribArray(1);
+	// 纹理坐标属性
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
+	// 释放
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+
+	GLuint texture;
+
+	int width, height;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	unsigned char *image = SOIL_load_image("res/images/image2.png", &width, &height, 0, SOIL_LOAD_RGBA);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
 
 
 	/* Loop until the user closes the window */
@@ -100,8 +137,12 @@ int main()
 		
 		ourShader.Use();
 
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture"), 0);
+
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		/* Swap front and back buffers */
@@ -111,6 +152,7 @@ int main()
 	}
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glfwTerminate();
 	return EXIT_SUCCESS;
 }
